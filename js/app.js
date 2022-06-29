de3c2caf1488a6e5ace401627384bb6c842eb208
Muhammad Ameen <mameen@nisum.com>
2022-06-29 18:58:37 +0500

    Complete

--- js/app.js
+++ js/app.js
@@ -4,8 +4,10 @@ const menu = document.getElementById("menu");
 const menu_li = document.querySelector(".menu_li");
 const body = document.getElementById("body");
 const complete_card = document.getElementById("complete_card");
+const result_count = document.getElementById("result_count");
+const scrollTop_button = document.getElementById("scrollTop_button");
 
-console.log(menu_li);
+scrollTop_button.style.display = "none";
 
 function showSubMenu() {
   submenu.style.display = "flex";
@@ -21,38 +23,58 @@ function hideSubMenu() {
 
 async function fetchText() {
   console.log("fn run");
-  let products = [];
+  let products = {};
 
-  await fetch("https://dummyjson.com/products/category/womens-dresses?limit=15") //api for the get request
+  await fetch("https://dummyjson.com/products/category/mens-shirts?limit=15") //api for the get request
     .then((response) => response.json())
-    .then((data) => (products = data.products));
+    .then((data) => (products = data));
   console.log(products);
   let html = "";
 
-  products.forEach((docs) => {
-    console.log(docs);
-    html += `<div class="col-lg-4"><div class="card_img">
-                <img src="${docs?.thumbnail}" alt="" />
-            </div>
-            <div class="multiple_img">
-            
-            <div class="multiple_img1">
-                <img src="./assets/images/img1.jpeg" alt="" />
-            </div>
-            <div class="multiple_img1"></div>
-            <div class="multiple_img1"></div>
-            </div>
-            <div class="titleAndDescription">
-                <p>${docs?.title}.</p>
-                <p>${docs?.description}</p>
-                <p>Limited-Time Special</p>
-                <p>PKR ${docs?.price}</p>
-                <p>Sale PKR 5,406.34</p>
-                <p>Discount: ${docs.discountPercentage}%</p>
-                <p>&#9733;&#9733;&#9733;&#9733;&#9733;</p>
-            </div></div>`;
+  products?.products.forEach((docs) => {
+    let salePrice =
+      docs?.price - (docs?.price * docs?.discountPercentage) / 100;
+    html += `<div class="col-lg-4 col-md-6 col-sm-12" style="margin-top: 30px;">
+              <div class="card_img">
+                  <img src="${docs?.thumbnail}" alt="loading" />
+              </div>
+              <div class="multiple_img">
+              
+              <div class="multiple_img1">
+                  <img src="./assets/images/img1.jpeg" alt="" />
+              </div>
+              <div class="multiple_img1"><img src="./assets/images/img1.jpeg" alt="" /></div>
+              <div class="multiple_img1"><img src="./assets/images/img1.jpeg" alt="" /></div>
+              </div>
+              <div class="titleAndDescription">
+                  <p>${docs?.title}.</p>
+                  <p>${docs?.description}</p>
+                  <p>Limited-Time Special</p>
+                  <p>PKR ${docs?.price}</p>
+                  <p>Sale <span style="color: #e01a2b; font-weight: bold;"> 
+                    PKR ${salePrice.toFixed(3)}
+                  </span></p>
+                  <p>Discount: ${docs.discountPercentage}%</p>
+                  <p>&#9733;&#9733;&#9733;&#9733;&#9733; ${docs?.rating}</p>
+              </div>
+            </div>`;
   });
   complete_card.innerHTML = html;
+  result_count.innerHTML = products?.total;
 }
 
 fetchText();
+
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
+
+function onScrollRun() {
+  if (window.pageYOffset > 200) {
+    scrollTop_button.style.display = "block";
+  } else {
+    scrollTop_button.style.display = "none";
+  }
+}
+
+console.log(window.pageYOffset);
